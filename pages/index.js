c5b5d84d29c6dd72fda34eaa1f559f2cad771a08
andredwi93 <andredwi93@gmail.com>
2022-07-06 17:34:41 +0700

    eslint

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,8 @@
 import Contact from "./components/Contact";
-import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Profile from "./components/Profile";
 import Projects from "./components/Projects";
 import Skill from "./components/Skill";
-import Slider from "./components/Slider";
-import { SliderData } from "./components/SliderData";
 export default function Home() {
   return (
     <>
